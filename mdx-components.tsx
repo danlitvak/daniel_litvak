785b9a6b2c0e92ae2dcad35ec0061bf3a9a759f0
danlitvak <daniel.litvak.64@gmail.com>
2025-11-22 17:25:44 -0800

    Improve blog feature demo styling

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -16,11 +16,22 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     }) => {
       return (
         <figure>
-          <img src={src} alt={alt} className="rounded-xl" />
+          <img
+            src={src}
+            alt={alt}
+            className="h-64 w-full rounded-xl object-cover shadow-sm md:h-80"
+          />
           <figcaption className="text-center">{caption}</figcaption>
         </figure>
       )
     },
+    table: ({ children, ...props }: ComponentPropsWithoutRef<'table'>) => (
+      <div className="overflow-x-auto rounded-xl border border-black/10 bg-white/40 shadow-sm dark:border-white/10 dark:bg-white/5">
+        <table className="w-full min-w-[480px] border-collapse text-sm" {...props}>
+          {children}
+        </table>
+      </div>
+    ),
     code: ({ children, ...props }: ComponentPropsWithoutRef<'code'>) => {
       const codeHTML = highlight(children as string)
       return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
